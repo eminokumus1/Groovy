be783fe39a1c6b672edcb61092b7e47d91e796fc
eminokumus1 <eminokumus1@gmail.com>
2024-09-10 16:22:43 +0300

    Mapper unit test added.

--- app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistMapper.kt
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistMapper.kt
@@ -1,11 +1,18 @@
 package petros.efthymiou.groovy.playlist
 
+import petros.efthymiou.groovy.R
 import petros.efthymiou.groovy.vo.Playlist
 import petros.efthymiou.groovy.vo.PlaylistRaw
 import javax.inject.Inject
 
 class PlaylistMapper @Inject constructor(): Function1<List<PlaylistRaw>, List<Playlist>> {
-    override fun invoke(p1: List<PlaylistRaw>): List<Playlist> {
-        TODO("Not yet implemented")
+    override fun invoke(playlistsRaw: List<PlaylistRaw>): List<Playlist> {
+        return playlistsRaw.map {
+            val image = when(it.category){
+                "rock" -> R.mipmap.rock
+                else -> R.mipmap.playlist
+            }
+            Playlist(it.id, it.name, it.category, image)
+        }
     }
 }
--- app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistMapperShould.kt
+++ app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistMapperShould.kt
@@ -6,26 +6,53 @@ import org.junit.Test
 import petros.efthymiou.groovy.utils.BaseUnitTest
 import petros.efthymiou.groovy.vo.Playlist
 import petros.efthymiou.groovy.vo.PlaylistRaw
+import petros.efthymiou.groovy.R
 
 class PlaylistMapperShould: BaseUnitTest() {
 
     private lateinit var mapper: PlaylistMapper
     private lateinit var playlistRaw: PlaylistRaw
+    private lateinit var playlistRawRock: PlaylistRaw
     private lateinit var playlists: List<Playlist>
     private lateinit var playlist: Playlist
+    private lateinit var playlistRock: Playlist
 
     @Before
     fun setup(){
         playlistRaw = PlaylistRaw("1", "name", "category")
+        playlistRawRock = PlaylistRaw("1", "name", "rock")
+
         mapper = PlaylistMapper()
+
         playlists = mapper(listOf(playlistRaw))
         playlist = playlists[0]
+        playlistRock = mapper(listOf(playlistRawRock))[0]
 
     }
 
     @Test
     fun keepSameId(){
         assertEquals(playlistRaw.id, playlist.id)
+    }
+
+    @Test
+    fun keepSameName(){
+        assertEquals(playlistRaw.name, playlist.name)
+    }
+
+    @Test
+    fun keepSameCategory(){
+        assertEquals(playlistRaw.category, playlist.category)
+    }
+
+    @Test
+    fun mapDefaultImageWhenNotRock(){
+        assertEquals(R.mipmap.playlist, playlist.image)
+    }
+
+    @Test
+    fun mapRockImageWhenRockCategory(){
+        assertEquals(R.mipmap.rock, playlistRock.image)
     }
 
 }
